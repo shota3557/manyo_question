c77f4ec57fdae7a54973e6cfc4d6584842d16e09
shota kikuchi <shota3557@gmail.com>
2022-10-28 18:12:45 +0900

    タイムゾーンを東京に変更した

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ Bundler.require(*Rails.groups)
 module Manyo
   class Application < Rails::Application
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 6.1
     config.generators do |g|
       g.test_framework :rspec,
