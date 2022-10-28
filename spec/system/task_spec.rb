ea0c2a4d8b49d1347f9bfa3d56db805afeadc5ce
shota kikuchi <shota3557@gmail.com>
2022-10-28 17:09:04 +0900

    README.mdを修正した

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -25,8 +25,8 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        @task = FactoryBot.create(:task, name: 'task')
-        visit task_path(@task.id)
+        task = FactoryBot.create(:task, name: 'task')
+        visit task_path(task.id)
         expect(page).to have_content 'task'
       end
     end
