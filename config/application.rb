1a3e3011fd76eebd275617220d5891c8c944a746
shota kikuchi <shota3557@gmail.com>
2022-10-28 18:00:44 +0900

    config.i18n.default_locale = :jaを追加した

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ Bundler.require(*Rails.groups)
 
 module Manyo
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.load_defaults 6.1
     config.generators do |g|
       g.test_framework :rspec,
