222138a3015c6c03d17935a87a63aafebbb62dd3
shota kikuchi <shota3557@gmail.com>
2022-10-31 14:52:22 +0900

    FactoryBotに終了期限のカラムを追加した

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -4,11 +4,13 @@ FactoryBot.define do
     factory :task do
       name { 'テスト１' }
       content { '内容１' }
+      end_date { '002022-11-10' }
     end
     # 作成するテストデータの名前を「second_task」とします
     # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
     factory :second_task, class: Task do
       name  { 'テスト２' }
       content { '内容２' }
+      end_date { '002022-11-11' }
     end
   end
