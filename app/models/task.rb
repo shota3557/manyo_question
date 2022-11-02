143c9070112110b04d438f2731d1585e8e2c1219
shota kikuchi <shota3557@gmail.com>
2022-11-02 11:31:35 +0900

    enumを追加した

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
+  scope :sort_expired, -> { order(end_date: :asc) }
+  enum status: { 未着手: 1, 着手中: 2, 完成: 3 }
 end
