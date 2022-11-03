053bc7acc6058b1db5a21fba7facb151e695492e
shota kikuchi <shota3557@gmail.com>
2022-11-03 14:39:01 +0900

    優先度のenumを追加した

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,7 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
   scope :sort_expired, -> { order(end_date: :asc) }
-  enum status: { 未着手: 1, 着手中: 2, 完成: 3 }
+  scope :rank, -> { order(priority: :desc) }
+  enum status: { 未着手: 0, 着手中: 1, 完成: 2 }
+  enum priority: { 低い: 0, 中: 1, 高い: 2 }
 end
