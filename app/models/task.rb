3df8b5dc65fbb4b48d9dda03667add624099032a
shota kikuchi <shota3557@gmail.com>
2022-11-04 14:42:06 +0900

    status_nameメソッドを実装した

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validates :content, presence: true
   scope :sort_expired, -> { order(end_date: :asc) }
   scope :rank, -> { order(priority: :desc) }
+  scope :status_name, -> (status){ where(status: status)}
   enum status: { 未着手: 1, 着手中: 2, 完成: 3 }
   enum priority: { 低い: 0, 中: 1, 高い: 2 }
 end
