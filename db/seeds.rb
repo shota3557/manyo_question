daf27e20e8ea07e943162aba90160274a7764c20
shota kikuchi <shota3557@gmail.com>
2022-11-09 14:08:23 +0900

    seedデータを変更した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-User.create!(name: "test", email: "test@test.com", password: "testtest", password_confirmation: "testtest" )
+User.create!(name: "test2", email: "test2@test.com", password: "testtest", password_confirmation: "testtest" )
 User.all.each do |user|
-    Task.create!(
-     user_id: user.id,
-     name: "title",
-     content: "test",
-     status: "着手中",
-     priority: "低い"
-     end_date: "2022/11/30" )
-   end
+  Task.create!(user_id: user.id, name: "test", content: "test", status: "着手中", priority: "低い", end_date: "002022/11/30")
+end
